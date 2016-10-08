73e2b2ace5620022861c2e8481efe1075fe732ff
DoubleGremlin181 <khukmani@gmail.com>
2016-10-08 14:22:55 +0530

    Update xkcd.py
    Allow users to choose comic number

--- XKCD/xkcd.py
+++ XKCD/xkcd.py
@@ -5,15 +5,18 @@ from io import BytesIO
 
 base_url = "http://xkcd.com/"
 
-for n in range(1, 3):
-    url = base_url + str(n)
-    page = requests.get(url).content
-    soup = BeautifulSoup(page, "lxml")
-    comicImageBlock = soup.find("div",{"id":"comic"})
-    comicImageTag = comicImageBlock.find("img")
-    comicURL = comicImageTag['src']
-    imageURL = 'https:' + comicURL
-    img_data = requests.get(imageURL)
-    i = Image.open(BytesIO(img_data.content))
-    save_file_name = 'xkcd' + str(n) + '.png'
-    i.save(save_file_name)
+n = raw_input("Enter the comic number\n> ")
+
+url = base_url + str(n)
+page = requests.get(url).content
+soup = BeautifulSoup(page, "lxml")
+comicImageBlock = soup.find("div",{"id":"comic"})
+comicImageTag = comicImageBlock.find("img")
+comicURL = comicImageTag['src']
+imageURL = 'https:' + comicURL
+img_data = requests.get(imageURL)
+i = Image.open(BytesIO(img_data.content))
+save_file_name = 'xkcd' + str(n) + '.png'
+i.save(save_file_name)
+
+print "XKCD"+str(n)+" has been saved successfully"
